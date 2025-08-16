2ddc2b2abb1c1066103b2f16bffc4be04969e4b0
Gift Udofiah <giftudofiah.com>
2025-08-16 05:27:01 +0100

    random done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-// src/App.js
-import React from 'react';
 import './App.scss';
 import QuoteMachine from './QuoteMachine';
 
 function App() {
+
+  
   return (
     <div>
+      <div className='title'> Gift's Random Quote Machine App </div>
       <QuoteMachine />
     </div>
   );
--- src/QuoteMachine.jsx
+++ src/QuoteMachine.jsx
@@ -1,36 +1,33 @@
-// src/QuoteMachine.js
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import quotes from './json files/quotes.json';
+import colors from './json files/colors.json';
+import { FaXTwitter } from "react-icons/fa6"; 
+
 
 const QuoteMachine = () => {
   const [quote, setQuote] = useState('');
   const [author, setAuthor] = useState('');
   const [backgroundColor, setBackgroundColor] = useState('#ffffff');
 
-  const fetchQuote = async () => {
-    try {
-      const response = await fetch('https://api.quotable.io/random');
-      const data = await response.json();
-      setQuote(data.content);
-      setAuthor(data.author);
-      changeBackgroundColor();
-    } catch (error) {
-      console.error('Error fetching quote:', error);
-    }
+  const fetchQuote = () => {
+    const random = quotes[Math.floor(Math.random() * quotes.length)];
+    setQuote(random.text);
+    setAuthor(random.author);
+    changeBackgroundColor();
   };
 
   const changeBackgroundColor = () => {
-    const colors = ['#FF5733', '#336BFF', '#33FF57', '#FF33C1', '#D533FF'];
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     setBackgroundColor(randomColor);
   };
-
+  
+ 
   useEffect(() => {
     fetchQuote();
   }, []);
 
   return (
-    <div id="fdiv" style={{ backgroundColor }} className={`random-bg`}>
-      <h2>Random Quote Machine</h2>
+    <div id='fdiv' style={{ backgroundColor }} className={`random-bg`}>
       <div id="quote-box">
         <div id="text">
           <blockquote>{quote}</blockquote>
@@ -48,14 +45,23 @@ const QuoteMachine = () => {
           rel="noopener noreferrer"
         >
           Tweet Quote
+          <FaXTwitter className='twit'/>
         </a>
       </div>
-      <div className='last'>
-        <p> Random Quote Machine App </p>
-        <p> Made with ❤ by Gift</p>
-      </div>
-      </div>
+      
+    </div>
   );
 };
 
 export default QuoteMachine;
+
+
+ // try {
+    //    const response = await fetch('https://zenquotes.io/api/random');
+    //   const data = await response.json();
+    //   setQuote(data.content);
+    //   setAuthor(data.author);
+    //   changeBackgroundColor();
+    // } catch (error) {
+    //   console.error('Error fetching quote:', error);
+    // }
